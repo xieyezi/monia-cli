3ac87e59d43bbcd0803ab026e8120985dd23c5d8
xieyezi <1435398529@qq.com>
2020-12-30 20:10:13 +0800

    feat: create flutter app function

--- lib/action/createFlutter.ts
+++ lib/action/createFlutter.ts
@@ -8,10 +8,61 @@ import logSymbols from 'log-symbols'
 import child_process from 'child_process'
 import { downloadFromGithub } from '../utils'
 import REMOTE_URL from '../value'
+const COMMON = 'burnish_temp'
+const regDescription = 'description: A new Flutter project.'
+const regVersion = 'version: 1.0.0+1'
 
-const createFlutterApp = (projectName: string, targetDir: string) => {
+const createFlutterApp = async (projectName: string, targetDir: string) => {
 	try {
-		child_process.execSync(`flutter create ${projectName}`, { stdio: [0, 1] })
+		const { description, version } = await inquirer.prompt([
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Please input your project description',
+				default: 'description',
+				validate(val) {
+					return true
+				}
+			},
+			{
+				type: 'input',
+				name: 'version',
+				message: 'Please input project version',
+				default: '1.0.0',
+				validate(val) {
+					return true
+				},
+				transformer(val) {
+					return val
+				}
+			}
+		])
+		child_process.execSync(`flutter create ${projectName}`, { stdio: [0, 1], windowsHide: true })
+		process.chdir(`./${projectName}`)
+		const spinner = Ora({
+			text: `Download template from burnish git repository... This might take a while....\n`
+		})
+		downloadFromGithub(REMOTE_URL.FLUTTER, COMMON).then((res) => {
+			fs.readFile(`./pubspec.yaml`, 'utf8', function(err, data) {
+				if (err) {
+					spinner.stop()
+					console.error(err)
+					return
+				}
+				let fileContent = data
+				fileContent = fileContent.replace(regVersion, `version: ${version}`)
+				fileContent = fileContent.replace(regDescription, `description: ${description}.`)
+				fs.writeFile(`./pubspec.yaml`, fileContent, 'utf8', function(err) {
+					if (err) {
+						console.error(err)
+					} else {
+						// console.log(chalk.white(`📦  Installing additional dependencies...\n`))
+						console.log('write success!')
+					}
+					process.exit()
+				})
+			})
+		})
 	} catch (error) {
 		console.log(error)
 	}
