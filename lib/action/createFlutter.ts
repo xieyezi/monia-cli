18bedcd7d897fb110bec52b267acaeed82ccdcc5
xieyezi <1435398529@qq.com>
2020-12-31 15:56:15 +0800

    feat: flutter function

--- lib/action/createFlutter.ts
+++ lib/action/createFlutter.ts
@@ -1,14 +1,17 @@
 import Ora from 'Ora'
 import path from 'path'
-import fs from 'fs-extra'
 import chalk from 'chalk'
+import shell from 'shelljs'
+import fs from 'fs-extra'
 import figlet from 'figlet'
 import inquirer from 'inquirer'
 import logSymbols from 'log-symbols'
 import child_process from 'child_process'
+
 import { downloadFromGithub } from '../utils'
 import REMOTE_URL from '../value'
-const COMMON = 'burnish_temp'
+
+const COMMON = 'fluttertemplate'
 const regDescription = 'description: A new Flutter project.'
 const regVersion = 'version: 1.0.0+1'
 
@@ -37,34 +40,58 @@ const createFlutterApp = async (projectName: string, targetDir: string) => {
 				}
 			}
 		])
+		// 一些提示 ···
+		console.log(chalk.white(`\n\n✨  Creating project in ${chalk.yellow(targetDir)}.`))
+		console.log(chalk.white(`\n🗃  Initializing git repository....\n`))
 		child_process.execSync(`flutter create ${projectName}`, { stdio: [0, 1], windowsHide: true })
 		process.chdir(`./${projectName}`)
+		// 切换当前目录到目标文件夹
+		console.log('111111111')
 		const spinner = Ora({
 			text: `Download template from burnish git repository... This might take a while....\n`
 		})
-		downloadFromGithub(REMOTE_URL.FLUTTER, COMMON).then((res) => {
-			fs.readFile(`./pubspec.yaml`, 'utf8', function(err, data) {
-				if (err) {
-					spinner.stop()
-					console.error(err)
-					return
-				}
-				let fileContent = data
-				fileContent = fileContent.replace(regVersion, `version: ${version}`)
-				fileContent = fileContent.replace(regDescription, `description: ${description}.`)
-				fs.writeFile(`./pubspec.yaml`, fileContent, 'utf8', function(err) {
-					if (err) {
-						console.error(err)
-					} else {
-						// console.log(chalk.white(`📦  Installing additional dependencies...\n`))
-						console.log('write success!')
-					}
-					process.exit()
-				})
+		spinner.start()
+		downloadFromGithub(REMOTE_URL.FLUTTER, COMMON)
+			.then((res) => {
+				console.log('res', res)
+				shell.rm('-f', './pubspec.yaml')
+				shell.rm('-rf', './lib')
+				shell.mv(`${targetDir}/${COMMON}/lib`, `${targetDir}`)
+				shell.mv(`${targetDir}/${COMMON}/pubspec.yaml`, `${targetDir}`)
+				spinner.stop()
 			})
-		})
+			.catch((err) => {
+				console.log(logSymbols.error, err)
+				spinner.fail(chalk.red('Sorry, it must be something error,please check it out. \n'))
+				process.exit(-1)
+			})
 	} catch (error) {
 		console.log(error)
 	}
 }
+
+// update pubspec.yaml file
+const updatePubspec = (spinner: Ora.Ora, description: string, version: string) => {
+	fs.readFile(`./pubspec.yaml`, 'utf8', function(err, data) {
+		if (err) {
+			spinner.stop()
+			console.error(err)
+			return
+		}
+		let fileContent = data
+		fileContent = fileContent.replace(regVersion, `version: ${version}`)
+		fileContent = fileContent.replace(regDescription, `description: ${description}.`)
+		fs.writeFile(`./pubspec.yaml`, fileContent, 'utf8', function(err) {
+			if (err) {
+				console.error(err)
+			} else {
+				// console.log(chalk.white(`📦  Installing additional dependencies...\n`))
+				console.log('write success!')
+			}
+			process.exit()
+		})
+	})
+}
+// move template file to targetDir
+const moveFile = () => {}
 export default createFlutterApp
--- lib/value.ts
+++ lib/value.ts
@@ -2,7 +2,7 @@
 const REMOTE_URL = Object.freeze({
 	VUE: 'https://github.com/xieyezi/vue-template.git',
 	REACT: 'https://github.com/xieyezi/react-template.git',
-	FLUTTER: 'https://github.com/xieyezi/flutter-Anniversary'
+	FLUTTER: 'https://github.com/xieyezi/flutter-template.git'
 })
 
 export default REMOTE_URL
