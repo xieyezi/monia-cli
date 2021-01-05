d4d6e18b77bda0f49d2bea9e0b9b4939ef7659e3
xieyezi <1435398529@qq.com>
2021-01-05 12:19:31 +0800

    feat: finish flutter create function

--- lib/action/createFlutter.ts
+++ lib/action/createFlutter.ts
@@ -4,9 +4,11 @@ import chalk from 'chalk'
 import shell from 'shelljs'
 import fs from 'fs-extra'
 import figlet from 'figlet'
+import replace from 'replace'
 import inquirer from 'inquirer'
 import logSymbols from 'log-symbols'
 import child_process from 'child_process'
+
 import { downloadFromGithub } from '../utils'
 import REMOTE_URL from '../value'
 
@@ -43,90 +45,99 @@ const createFlutterApp = async (projectName: string, targetDir: string) => {
 		console.log(chalk.white(`\n\n✨  Creating project in ${chalk.yellow(targetDir)}.`))
 		console.log(chalk.white(`\n🗃  Initializing git repository....\n`))
 		child_process.execSync(`flutter create ${projectName}`, { stdio: [0, 1], windowsHide: true })
-		process.chdir(`./${projectName}`)
 		// 切换当前目录到目标文件夹
-		console.log('111111111')
+		process.chdir(`./${projectName}`)
 		const spinner = Ora({
 			text: `Download template from burnish git repository... This might take a while....\n`
 		})
-		spinner.start()
-		downloadFromGithub(REMOTE_URL.FLUTTER, COMMON)
-			.then((res) => {
-				updateTargetFile(targetDir, spinner, description, version)
-			})
-			.catch((err) => {
-				console.log(err)
-				if (fs.existsSync(`${targetDir}/${COMMON}`)) {
-					updateTargetFile(targetDir, spinner, description, version)
-				} else {
-					console.log(logSymbols.error, err)
-					spinner.fail(chalk.red('Sorry, it must be something error,please check it out. \n'))
-					process.exit(-1)
-				}
-			})
+		// spinner.start()
+		await downloadFromGithub(REMOTE_URL.FLUTTER, COMMON).catch((err) => {
+			console.log(logSymbols.error, err)
+			spinner.fail(chalk.red('Sorry, it must be something error,please check it out. \n'))
+			process.exit(-1)
+		})
+		targetFileDisplayReplace(projectName, `${targetDir}/${COMMON}`)
+		updateTargetFile(projectName, targetDir, description, version, spinner)
 	} catch (error) {
 		console.log(error)
 	}
 }
-
-// update target file
-const updateTargetFile = (targetDir: string, spinner: Ora.Ora, description: string, version: string) => {
-	shell.rm('-f', './pubspec.yaml')
-	shell.rm('-rf', './lib')
-	console.log('2222222')
-	shell.mv(`${targetDir}/${COMMON}/lib`, `${targetDir}`)
-	shell.mv(`${targetDir}/${COMMON}/pubspec.yaml`, `${targetDir}`)
-	fs.readFile('./pubspec.yaml', 'utf8', function(err, data) {
-		if (err) {
-			spinner.stop()
-			console.error(err)
-			return
+// trave template and replace projectName
+const targetFileDisplayReplace = (projectName: string, filePath: string) => {
+	const files = fs.readdirSync(filePath, 'utf-8')
+	files.forEach(function(filename) {
+		let filedir = path.join(filePath, filename)
+		if (fs.statSync(filedir).isFile() && filedir.includes('.dart')) {
+			console.log(filedir)
+			replace({
+				regex: COMMON,
+				replacement: projectName,
+				paths: [filedir],
+				recursive: true,
+				silent: true
+			})
+		} else {
+			if (fs.statSync(filedir).isDirectory()) targetFileDisplayReplace(projectName, filedir) //递归，如果是文件夹，就继续遍历该文件夹下面的文件
 		}
-		let fileContent = data
-		fileContent = fileContent.replace(regVersion, `version: ${version}`)
-		fileContent = fileContent.replace(regDescription, `description: ${description}.`)
-		fs.writeFile(`./pubspec.yaml`, fileContent, 'utf8', function(err) {
-			if (err) {
-				console.error(err)
-			} else {
-				targetFileDisplayReplace('./lib')
-				spinner.stop()
-				console.log('end.....')
-			}
-			process.exit()
-		})
 	})
 }
 
-const targetFileDisplayReplace = (filePath: string) => {
-	console.log(filePath)
-	//根据文件路径读取文件，返回文件列表
-	fs.readdir(filePath, 'utf8', function(err, files) {
-		if (err) {
-			console.error(err)
-		} else {
-			console.log(files)
-			files.forEach(function(filename) {
-				//获取当前文件的绝对路径
-				let filedir = path.join(filePath, filename)
-				//根据文件路径获取文件信息，返回一个fs.Stats对象
-				fs.stat(filedir, function(eror, stats) {
-					if (eror) {
-						console.warn('获取文件stats失败')
-					} else {
-						let isFile = stats.isFile() //是文件
-						let isDir = stats.isDirectory() //是文件夹
-						if (isFile) {
-							console.log(filedir)
-						}
-						if (isDir) {
-							targetFileDisplayReplace(filedir) //递归，如果是文件夹，就继续遍历该文件夹下面的文件
-						}
-					}
-				})
-			})
-		}
+// update target file
+const updateTargetFile = async (
+	projectName: string,
+	targetDir: string,
+	description: string,
+	version: string,
+	spinner: Ora.Ora
+) => {
+	shell.rm('-rf', `${targetDir}/lib`)
+	shell.rm('-f', `${targetDir}/pubspec.yaml`)
+	shell.mv(`${targetDir}/${COMMON}/lib`, `${targetDir}`)
+	shell.mv(`${targetDir}/${COMMON}/pubspec.yaml`, `${targetDir}`)
+	await replace({
+		regex: regVersion,
+		replacement: `version: ${version}`,
+		paths: [`${targetDir}/pubspec.yaml`],
+		recursive: true,
+		silent: true
 	})
+	await replace({
+		regex: regDescription,
+		replacement: `description: ${description}.`,
+		paths: [`${targetDir}/pubspec.yaml`],
+		recursive: true,
+		silent: true
+	})
+	await fs.remove(`${targetDir}/${COMMON}`)
+	child_process.execSync('flutter pub get', { stdio: [0, 1], windowsHide: true })
+	spinner.stop()
+	// 依赖安装完成之后给出提示信息
+	console.log(chalk.white(`\n🎉  Successfully created project`), chalk.yellow(`${projectName}.`))
+	console.log(chalk.white('👉  Get started with the following commands:\n'))
+	console.log(`${chalk.cyan(`${chalk.gray('$')} cd ${projectName}`)}`)
+	console.log(chalk.cyan(`${chalk.gray('$')} flutter run\n\n`))
+	console.log(chalk.white(figlet.textSync('brnish-cli')))
+	process.exit()
+	// fs.readFile('./pubspec.yaml', 'utf8', function(err, data) {
+	// 	if (err) {
+	// 		spinner.stop()
+	// 		console.error(err)
+	// 		return
+	// 	}
+	// 	let fileContent = data
+	// 	fileContent = fileContent.replace(regVersion, `version: ${version}`)
+	// 	fileContent = fileContent.replace(regDescription, `description: ${description}.`)
+	// 	fs.writeFile(`./pubspec.yaml`, fileContent, 'utf8', function(err) {
+	// 		if (err) {
+	// 			console.error(err)
+	// 		} else {
+	// 			targetFileDisplayReplace('lib')
+	// 			spinner.stop()
+	// 			console.log('end.....')
+	// 		}
+	// 		process.exit()
+	// 	})
+	// })
 }
 
 export default createFlutterApp
