3a56c54268ef48b915b09c9abfa009b9d58f9de4
xieyezi <1435398529@qq.com>
2021-06-05 16:21:13 +0800

    feat: finish vscode extension

--- vscode-extension/src/comands/generate.ts
+++ vscode-extension/src/comands/generate.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs-extra'
 import * as vscode from 'vscode'
-import {pageView, pageController, pageBindings, pageModel} from '../template'
-import { promptForPageName, toBigHump, toBottomLine,makeDirSync,writeFileSync } from '../utils'
+import { pageView, pageController, pageBindings, pageModel } from '../template'
+import { promptForPageName, toBottomLine, makeDirSync, writeFileSync } from '../utils'
 
 export default async function generateNewGetxPage() {
 	const pageName = await promptForPageName()
@@ -12,24 +12,24 @@ export default async function generateNewGetxPage() {
 
 	const pageLineName = toBottomLine(pageName)
 	const targetDirectory = vscode.workspace.rootPath
-    const pageLibIsExist =fs.existsSync(`${targetDirectory}/lib/pages`)
-    if(!pageLibIsExist) {
-        vscode.window.showErrorMessage(`The ${targetDirectory}/lib/pages lib must not be empty`)
+	const pageLibIsExist = fs.existsSync(`${targetDirectory}/lib/pages`)
+	if (!pageLibIsExist) {
+		vscode.window.showErrorMessage(`The ${targetDirectory}/lib/pages lib must not be empty`)
 		return
-    }
-    const targetPageLib = `${targetDirectory}/lib/pages/${pageLineName}`
-    // 创建目录
-    makeDirSync(targetPageLib)
-    const pageViewContent = pageView(pageName)
-    const pageControllerContent = pageController(pageName)
-    const pageBindingsContent = pageBindings(pageName)
-    const pageModelContent = pageModel()
-    // 写入文件内容
-    writeFileSync(`${targetPageLib}/${pageLineName}_view.dart`, pageViewContent)
-    writeFileSync(`${targetPageLib}/${pageLineName}_controller.dart`, pageControllerContent)
-    writeFileSync(`${targetPageLib}/${pageLineName}_binding.dart`, pageBindingsContent)
-    writeFileSync(`${targetPageLib}/${pageLineName}_model.dart`, pageModelContent)
+	}
+	const targetPageLib = `${targetDirectory}/lib/pages/${pageLineName}`
+	// 创建目录
+	makeDirSync(targetPageLib)
+	const pageViewContent = pageView(pageName)
+	const pageControllerContent = pageController(pageName)
+	const pageBindingsContent = pageBindings(pageName)
+	const pageModelContent = pageModel()
+	// 写入文件内容
+	writeFileSync(`${targetPageLib}/${pageLineName}_view.dart`, pageViewContent)
+	writeFileSync(`${targetPageLib}/${pageLineName}_controller.dart`, pageControllerContent)
+	writeFileSync(`${targetPageLib}/${pageLineName}_binding.dart`, pageBindingsContent)
+	writeFileSync(`${targetPageLib}/${pageLineName}_model.dart`, pageModelContent)
 
-    // console.log(chalk.white(`\n🎉  Successfully generate page`), chalk.yellow(`${pageName}.`))
-
+	// console.log(chalk.white(`\n🎉  Successfully generate page`), chalk.yellow(`${pageName}.`))
+	vscode.window.showInformationMessage(`🎉 Successfully generate page ${pageName}.`)
 }
--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -3,16 +3,13 @@ import { generateStatusBar } from './utils'
 import generateNewGetxPage from './comands/generate'
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
-	generateStatusBar('monia-create', 'monia:create')
 	generateStatusBar('monia-generate', 'monia:generate')
-
-	const create = vscode.commands.registerCommand('monia:create', () => {})
-
 	const generate = vscode.commands.registerCommand('monia:generate', generateNewGetxPage)
-
 	subscriptions.push(generate)
 
 	//TODO: 完成项目新建命令
+	// generateStatusBar('monia-create', 'monia:create')
+	// const create = vscode.commands.registerCommand('monia:create', () => {})
 	//subscriptions.push(create)
 }
 
